43e62b5dd07a63d6cbf58cd326aefebcc5d55b71
RanjanBa <ranjanbaro2015@gmail.com>
2024-08-08 16:34:36 +0530

    fixing naming

--- Assets/Python/kyber_encrypt_decrypt.py
+++ Assets/Python/kyber_encrypt_decrypt.py
@@ -8,21 +8,20 @@ from kyber import Kyber1024
 
 def getKey():
     public_key, private_key = Kyber1024.keygen()
-    
     return (public_key, private_key)
 
 
 def encrypt(msg : str, public_key : bytes): 
-    cipher_encrypt, shared_key = Kyber1024.enc(public_key)
-
+    encapsulated_text, shared_key = Kyber1024.enc(public_key)
     # Encrypt the data with the AES session key
     encoded_msg = msg.encode("utf-8")
     cipher_aes = AES.new(shared_key, AES.MODE_EAX)
     cipher_text, tag = cipher_aes.encrypt_and_digest(encoded_msg)
-    return (cipher_encrypt, cipher_text, tag, cipher_aes.nonce)
-    
-def decrypt(private_key : bytes, cipher_encrypt : bytes, cipher_text : bytes, tag : bytes, nonce : bytes):
-    shared_key = Kyber1024.dec(cipher_encrypt, private_key)
+    return (encapsulated_text, cipher_text, tag, cipher_aes.nonce)
+
+
+def decrypt(private_key : bytes, encapsulated_text : bytes, cipher_text : bytes, tag : bytes, nonce : bytes):
+    shared_key = Kyber1024.dec(encapsulated_text, private_key)
     # Decrypt the data with the AES session key
     cipher_aes = AES.new(shared_key, AES.MODE_EAX, nonce)
     data = cipher_aes.decrypt_and_verify(cipher_text, tag)
@@ -40,10 +39,20 @@ def main():
 
     pb_key, pvt_key = getKey()
     encapsulated_text, cipher_text, tag, nonce =  encrypt(msg, pb_key)
-    # print(encapsulated_text)
-    decode_msg = decrypt(pvt_key, encapsulated_text, cipher_text, tag, nonce)
+    print("Encrypt 1 : ", end="")
+    print(encapsulated_text)
     print("cipher text : ")
     print(bytes.hex(cipher_text))
+    decode_msg = decrypt(pvt_key, encapsulated_text, cipher_text, tag, nonce)
+    print("Decrypted msg...")
+    print(decode_msg)
+    
+    encapsulated_text, cipher_text, tag, nonce =  encrypt(msg, pb_key)
+    print("Encrypt 2 : ", end="")
+    print(encapsulated_text)
+    print("cipher text : ")
+    print(bytes.hex(cipher_text))
+    decode_msg = decrypt(pvt_key, encapsulated_text, cipher_text, tag, nonce)
     print("Decrypted msg...")
     print(decode_msg)
 
